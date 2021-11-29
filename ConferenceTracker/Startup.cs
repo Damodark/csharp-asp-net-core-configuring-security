4af8e0f3207de897a6bce9710cfe3cdde2b8e94e
Damodar Karmacharya <d_karmacharya@hotmail.com>
2021-11-30 09:46:20 +1100

    These are new changess..

--- ConferenceTracker/Startup.cs
+++ ConferenceTracker/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 
 namespace ConferenceTracker
@@ -18,6 +19,7 @@ namespace ConferenceTracker
         {
             Configuration = configuration;
         }
+        private string _allowedOrigins { get; set; } = "_allowedOrigins";
 
         public IConfiguration Configuration { get; }
         public string SecretMessage { get; set; }
@@ -29,22 +31,41 @@ namespace ConferenceTracker
             services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("ConferenceTracker"));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.Configure<CookiePolicyOptions>(option => { option.CheckConsentNeeded = context => true; option.MinimumSameSitePolicy = SameSiteMode.None; });
             services.AddControllersWithViews();
+            services.AddCors(options => { options.AddPolicy(_allowedOrigins, builder => { builder.WithOrigins("http://pluralsight.com"); }); });
             services.AddRazorPages();
             services.AddTransient<IPresentationRepository, PresentationRepository>();
             services.AddTransient<ISpeakerRepository, SpeakerRepository>();
+            SecretMessage = Configuration["SecretMessage"];
+
+
         }
 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {            
             using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
             using (var context = scope.ServiceProvider.GetService<ApplicationDbContext>())
                 context.Database.EnsureCreated();
 
             app.UseStaticFiles();
+            app.UseHttpsRedirection();
 
+            app.UseCors(_allowedOrigins);
+            if (!env.IsDevelopment())
+            {
+                app.UseHsts();
+                app.UseExceptionHandler("/Home/Error");
+            }
+            else
+            {
+                logger.LogInformation("Environment is in development");
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            app.UseCookiePolicy();
             app.UseRouting();
 
             app.UseAuthentication();
